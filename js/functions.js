4807ff85e60a52ed58c1e27893371b563a9b1c61
GrandDebatAnalyse <48725322+GrandDebatAnalyse@users.noreply.github.com>
2019-04-10 14:29:13 +0200

    deleted files

--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,7 @@ var lis = liContainer.getElementsByClassName("li_pres_subjects_text");
 for (var i = 0; i < lis.length; i++) {
   lis[i].addEventListener("click", function() {
     var current = liContainer.getElementsByClassName("active");
-    if (current.length==0 | this!=current[0]) {
+    if (current.length==0 || this!=current[0]) {
     	this.className += " active";
     }
     if (current.length==1){

d9242944915b4ef0076cc9f4915e22915ada3222
GrandDebatAnalyse <48725322+GrandDebatAnalyse@users.noreply.github.com>
2019-04-13 13:25:42 +0200

    BarPlots

--- js/functions.js
+++ js/functions.js
@@ -17,3 +17,25 @@ for (var i = 0; i < lis.length; i++) {
     RightDivToChange.className += " active";
   });
 }
+
+
+// For the BarPLot Section
+// Get the container element
+var liContainer_BarPlots = document.getElementById("list_BarPlots");
+// Get all buttons with class="btn" inside the container
+var div_BarPlots = liContainer_BarPlots.getElementsByClassName("li_BarPlots_subjects_text");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < div_BarPlots.length; i++) {
+  div_BarPlots[i].addEventListener("click", function() {
+    var current = liContainer_BarPlots.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    var currentTextRight = document.getElementById("text_BarPlots_right").getElementsByClassName("active");
+    currentTextRight[0].className = currentTextRight[0].className.replace(" active", "");
+
+    var liContainerRight = document.getElementById("text_BarPlots_right");
+    var RightDivToChange = liContainerRight.getElementsByClassName(this.id)[0]
+    RightDivToChange.className += " active";
+  });
+}
